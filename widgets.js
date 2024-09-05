796a6a1e44910cb3c1ac6e6c6ca976fecc51f231
Robyn011 <robyncocopumpoen@gmail.com>
2024-09-05 14:13:58 +0200

    adding images and some code

---
+++ widgets.js
@@ -0,0 +1,18 @@
+let bootcamp_name = ["CodeSpace", "Univercity of Cape Town", "Stellenbosh", "Univercity of Western Cape", "CapaCiti"];
+let onsite = [ "both", true, true, true, true, "both"]
+let image = ["/assets/codespaceLogo.jpg"]
+if (onsite = true){
+  control.log("Onsite");
+
+} else if (onsite = "both"){
+  control.log("Online/Onsite")
+} else{
+  control.log("Online")
+}
+
+
+// Still in progress
+
+
+
+
